360a432627fd43c1082cfa7790e77918ec1ce77c
Anderson Cardoso <ancardos@Andersons-MacBook-Pro.local>
2018-09-21 11:45:30 -0400

    add new files

---
+++ env.sh
@@ -0,0 +1,13 @@
+# Authentication detail
+export TF_VAR_tenancy_ocid=""
+export TF_VAR_user_ocid=""
+export TF_VAR_fingerprint=""
+export TF_VAR_private_key_path=""
+
+# Region
+export TF_VAR_region=""
+# Compartmenet
+export TF_VAR_compartment_ocid=""
+# Public/Private key used in instance
+export TF_VAR_ssh_public_key=""
+export TF_VAR_ssh_private_key=""
---
+++ output.tf
@@ -0,0 +1,3 @@
+/*
+Output variables provide a means to support Terraform end-user queries. This allows users to extract meaningful data from among the potentially massive amount of data associated with a complex infrastructure
+*/
