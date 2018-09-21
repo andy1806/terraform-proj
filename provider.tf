4738da615894328988b8674afe29d15730e98090
Anderson Cardoso <ancardos@Andersons-MacBook-Pro.local>
2018-09-21 11:39:20 -0400

    add variables and and provider file

--- provider.tf
+++ provider.tf
@@ -1,13 +1,20 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
+# Terraform variable declaraation; parameters for terraform modules
 
+# Authentication variables
+variable tenancy_ocid = {}
+variable user_ocid = {}
+variable fingerprint = {}
+variable private_key_path = {}
+# Region
+variable region = {}
+# Compartment
+variable compartment_ocid = {}
+
+# OCI provider definition
 provider "oci" {
-   tenancy_ocid = "${var.tenancy_ocid}"
-   user_ocid = "${var.user_ocid}"
-   fingerprint = "${var.fingerprint}"
-   private_key_path = "${var.private_key_path}"
-   region = "${var.region}"
+  region =  "${var.region}"
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${}"
+  private_key_path = "${var.private_key_path}"
 }
